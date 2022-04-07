54ad435d2a71bae3b1fe7903d6bd7514866b51f7
amro-mansour <amro8000@gmail.com>
2022-04-07 11:46:53 +0200

    Adding comments to my code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// The following code imports the Express module to then be able to use it withi  the app
 const express = require('express'),
- morgan = require('morgan');
+ morgan = require('morgan'); // the morgan library is imported to be able to log any information required
 
+// Here Express is set to a variable "app" to then use its functionalities throughout the app
 const app = express();
 
+// The following function routes any requests for static files to their corresponding file within the 'public' folder on the server
 app.use(express.static('public'));
 app.use(morgan('common'));
 
-
+// A list of my favourite movies
 let topMovies = [
   {
     title: 'Inception',
@@ -50,7 +53,7 @@ let topMovies = [
   }
 ];
 
-
+// the following three functions use Express "app" to make requests to the server using GET, the type of data returned depends on the endpoint of the path entered in the URL
 app.get('/documentation.html', (req, res) => {
   res.sendFile('documentation.html', { root: __dirname });
 });
@@ -63,6 +66,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to my movies library!')
 })
 
+// This code would execute every time an error occurs in the code (that hasn't already been handled elsewhere)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
