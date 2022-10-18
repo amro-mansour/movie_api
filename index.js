486ac993c9c384b6faeea6561639db2e61fa8556
amro-mansour <amro8000@gmail.com>
2022-10-18 20:11:14 +0200

    Updating cors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
   morgan = require('morgan'); // the morgan library is imported to be able to log any information required
-const cors = require('cors');
 
 const cors = require('cors'); // Importing CORS into the app to then allow the domain specified have access to the app
 
@@ -25,9 +24,18 @@ mongoose.connect(process.env.CONNECTION_URI, {
 });
 
 // CORS
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-movie-library.netlify.app/'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://myflix-movie-library.netlify.app/'];
 
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
